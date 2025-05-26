b236c7be1b38d34b32f298c34c65ece4fec911d1
Zenas97 <crccr@hotmail.it>
2025-05-26 23:27:35 +0200

    feat : Test railway config

--- backend/app.py
+++ backend/app.py
@@ -212,3 +212,8 @@ if load_models():
 else:
     print("Failed to load models. Exiting.")
     exit(1)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
